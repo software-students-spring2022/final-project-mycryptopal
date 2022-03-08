c1a3ea40349bd88f574cbe6f8ed28fada63ddbd5
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-03-07 19:02:30 -0500

    Created skeleton code and routes for Portfolio and Settings pages. Configured links in sidebar to redirect where they're supposed to

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Sidebar from './Sidebar';
 import Home from './Home';
+import Portfolio from './Portfolio';
 import Explore from './Explore';
 import Learn from './Learn';
 import Contact from './Contact';
+import Settings from './Settings';
 
 function App() {
   return (
@@ -14,9 +16,11 @@ function App() {
         <Router>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/portfolio' element={<Portfolio />} />
             <Route path='/explore' element={<Explore />} />
             <Route path='/learn' element={<Learn />} />
             <Route path='/contact' element={<Contact />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </Router>
       </div>
--- front-end/src/Sidebar.js
+++ front-end/src/Sidebar.js
@@ -5,11 +5,11 @@ function Sidebar() {
     return (
         <Menu>
             <a id="home" className="menu-item" href="/"><h1>MyCryptoPal</h1></a>
-            <a id="portfolio" className="menu-item" href="/">My Portfolio</a>
-            <a id="explore" className="menu-item" href="/">Explore Cryptos</a>
-            <a id="learn" className="menu-item" href="/">Learn</a>
-            <a id="contact" className="menu-item" href="/">Contact Us</a>
-            <a id="settings" className="menu-item" href="/">Settings</a>
+            <a id="portfolio" className="menu-item" href="/portfolio">My Portfolio</a>
+            <a id="explore" className="menu-item" href="/explore">Explore Cryptos</a>
+            <a id="learn" className="menu-item" href="/learn">Learn</a>
+            <a id="contact" className="menu-item" href="/contact">Contact Us</a>
+            <a id="settings" className="menu-item" href="/settings">Settings</a>
             <a id="logout" className="menu-item" href="/">Logout</a>
         </Menu>
     );
