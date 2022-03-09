0e1ea28d85e3d062d5b5a6d680bf0e6975368f25
Hannah Harvard <hh1927@nyu.edu>
2022-03-09 13:15:10 -0500

    added buttons to login page

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -2,7 +2,21 @@ import './Login.css';
 
 function Login() {
     return (
+    <div className="login">
         <h1>Login</h1>
+        <button
+            onClick={() => navigateToPage('/home')} //need to decide on page after login
+            className="page-button"
+        >
+            Login
+        </button>
+        <button
+            onClick={() => navigateToPage('/registration')} //need to talk to backend 
+            className="page-button"
+        >
+            Create Account
+        </button>
+    </div>
     )
 }
 
