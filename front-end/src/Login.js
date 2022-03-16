1399ca536528510d3f31900767dcea59d6f7e0a3
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-03-16 17:38:57 -0400

    Styled Login and Registration pages. Replaced for property with htmlFor in JSX input elements

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -2,23 +2,33 @@ import './Login.css';
 
 function Login() {
     return (
-    <div className="login">
-        <h1>Login</h1>
-        <div>
-            <form>
-                <label for="username">Username:</label>
-                <input name="username" type="text"></input>
-                <label for="password">Password:</label>
-                <input name="password" type="text"></input>
-                <a href = '/home'>
-                <input id="loginButton" type="button" value="Login"></input>
-                </a>
-            </form>
-        </div>
-        <a href = '/registration'>
-        <button className= "page-button"> Create Account </button>
-        </a>
-    </div>
+        <>
+            <div id="page-title">
+                <h1>Ready to Level Up Your Investing Career?</h1>
+
+                <div id="logo">
+                    <img src="https://picsum.photos/300" alt="MyCryptoPal Logo"/>
+                </div>
+
+                <div id="title">
+                    <h2>Login</h2>
+                </div>
+            </div>
+
+            <div id="page-content">
+                <div id="login">
+                    <form>
+                        <label htmlFor="username">Username</label>
+                        <input name="username" type="text" className="credentials"></input>
+                        <label htmlFor="password">Password</label>
+                        <input name="password" type="password" className="credentials"></input>
+                        <input onClick={() => window.location.href='/'} type="button" value="Login" className="formButton"></input>
+                        <h2>Don't Have An Account?</h2>
+                        <input onClick={() => window.location.href='/registration'} type="button" value="Create Account" className="formButton"></input>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
