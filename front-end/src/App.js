8a2f29db5a682e3d05bb5a05350e846bc52979dc
Hannah Harvard <hh1927@nyu.edu>
2022-03-21 11:58:51 -0400

    added lesson page and lesson map with all content

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import Portfolio from './Portfolio';
 import Explore from './Explore';
 import Learn from './Learn';
+import LearnMap from './LearnMap';
 import Contact from './Contact';
 import Settings from './Settings';
 
@@ -17,7 +18,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/portfolio' element={<Portfolio />} />
           <Route path='/explore' element={<Explore />} />
-          <Route path='/learn' element={<Learn />} />
+          <Route path='/learnMap' element={<LearnMap />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/settings' element={<Settings />} />
         </Routes>
--- front-end/src/Learn.js
+++ front-end/src/Learn.js
@@ -2,15 +2,23 @@ import './Learn.css';
 
 function Learn() {
     return (
-        <>
-            <div id="page-title">
-                <h1>Learn Page</h1>
-            </div>
-
-            <div id="page-content">
-            
-            </div>
-        </>
+    <div className='learn'>
+        <h1>Lesson Example</h1>
+        <div>
+        <div className="question"> Quiz Question 1 </div>
+            <form>
+                <label for="answer">Answer:</label>
+                <input name="answer" type="text"></input>
+                <input id="checkAnswerButton" type="button" value="Check"></input> 
+            </form>
+        </div>
+        <a href = '/learn'>
+        <button className= "page-button"> Previous Lesson </button>
+        </a>
+        <a href = '/learn'>
+        <button className= "page-button"> Next Lesson </button>
+        </a>
+    </div>
     )
 }
 
