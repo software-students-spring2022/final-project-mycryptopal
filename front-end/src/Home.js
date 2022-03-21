b4242dfb450735d164045902eb449b2f87592ca6
Blake Rainey <brrainey13@gmail.com>
2022-03-21 18:04:05 -0400

    implemented articles and buttons, having trouble with infinite scroll

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -106,46 +106,37 @@ function Home() {
           </div>
         </div>
         <div className="firstarticle">  
+            <a href="/article">
             <img id="photo1" src= {"https://picsum.photos/120"}></img>
-            <div className="text1"> 
+            </a>
             <p id="paragraph1"> 
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ac tincidunt vitae semper quis lectus. Dolor sit amet consectetur 
-            adipiscing elit pellentesque.Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ac tincidunt vitae semper quis lectus. Dolor sit amet consectetur 
             adipiscing elit pellentesque.
             </p>
-            </div>
         </div>
         <div className="secondarticle">  
+            <a href="article">
             <img id="photo2" src= {"https://picsum.photos/120"}></img>
-            <div className="text2">
+            </a>
             <p id="paragraph2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ac tincidunt vitae semper quis lectus. Dolor sit amet consectetur 
-            adipiscing elit pellentesque.Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ac tincidunt vitae semper quis lectus. Dolor sit amet consectetur 
             adipiscing elit pellentesque.
             </p>
-            </div>
         </div>
         <div className="thirdarticle">  
+            <a href="/article">
             <img id="photo3" src= {"https://picsum.photos/120"}></img>
-            <div className="text3">
+            </a>
             <p id="paragraph3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ac tincidunt vitae semper quis lectus. Dolor sit amet consectetur 
-            adipiscing elit pellentesque.Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ac tincidunt vitae semper quis lectus. Dolor sit amet consectetur 
             adipiscing elit pellentesque.
             </p>
-            </div>
         </div>
     </>
 
