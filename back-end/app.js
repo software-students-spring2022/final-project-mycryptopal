22605e60305f29cbd590d8d8c412e888104a25f0
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-04-03 22:01:28 -0400

    Moved API calls for cryptocurrency price graphs to backend

--- back-end/app.js
+++ back-end/app.js
@@ -24,45 +24,6 @@ app.get('/', (req, res) => {
     res.send('Example route');
 });
 
-app.get('/portfolio/:symbol', (req, res, next) => {
-    
-    const API_TOKEN = "c8qd4eiad3ienapjjc9g";
-    const CRYPTO_SYMBOLS = ["BINANCE:BTCUSDT", "BINANCE:ETHUSDT", "BINANCE:DOGEUSDT", "SOL", "BINANCE:SHIBUSDT"]
-    const INTERVAL_OPTIONS = [30, 60, 90, 120]
-    const API_URL = "https://finnhub.io/api/v1/crypto/candle"
-    const RESOLUTION = "D"
-
-    function getUnixTime(date) {
-        return date.getTime() / 1000 | 0;
-      }
-      
-      function transformData(data) {
-        return data.c.map((item, index) => ({
-          close: Number(item).toFixed(2),
-          open: Number(data.o[index]).toFixed(2),
-          timestamp: new Date(data.t[index] * 1000).toLocaleDateString()
-        }))
-      }
-
-        function to() {
-          return getUnixTime(new Date())
-      }
-        function from() {
-        let d = new Date();
-        d.setDate(d.getDate() - INTERVAL_OPTIONS[0]); //this will be modified to whatver is passed from front end
-        return getUnixTime(d);
-      }
-
-
-    
-      axios
-      .get(`https://finnhub.io/api/v1/crypto/candle?from=${from()}&resolution=D&symbol=${req.params.symbol}&to=${to()}&token=c8qd4eiad3ienapjjc9g`)
-      .then(apiResponse => res.json(apiResponse.data))
-      .catch(err => next(err));
-
-
-});
-
 app.get('/userdata', (req, res) => {
   axios
   .get(`https://my.api.mockaroo.com/users.json?key=4c156a80&limit=1`)
@@ -75,6 +36,48 @@ app.get('/crypto/:symbol', (req, res) => {
   .get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${req.params.symbol}`, { headers: { 'X-CMC_PRO_API_KEY': '528926c7-6726-45a1-9add-dcb670893b40' }})
   .then(apiResponse => res.json(apiResponse.data))
   .catch(err => console.log(err))
+});
+
+app.get('/graph/:symbol/:interval?', (req, res) => {
+
+  const API_TOKEN = "c8qd4eiad3ienapjjc9g";
+  const API_URL = "https://finnhub.io/api/v1/crypto/candle"
+  const symbol = req.params.symbol;
+  let querySymbol = 'BINANCE:'
+  let queryInterval = req.params.interval;
+
+  if (symbol === 'USDT') {
+    querySymbol += 'BUSDUSDT';
+  }
+  else {
+    querySymbol += `${symbol.toUpperCase()}USDT`;
+  }
+
+  if (!queryInterval) {
+    queryInterval = 30;
+  }
+  else {
+    queryInterval = parseInt(queryInterval);
+  }
+
+  function getUnixTime(date) {
+      return date.getTime() / 1000 | 0;
+  }
+
+  function to() {
+    return getUnixTime(new Date());
+  }
+  
+  function from() {
+    let d = new Date();
+    d.setDate(d.getDate() - queryInterval); //this will be modified to whatver is passed from front end
+    return getUnixTime(d);
+  }
+
+  axios
+  .get(`${API_URL}?from=${from()}&resolution=D&symbol=${querySymbol}&to=${to()}&token=${API_TOKEN}`)
+  .then(apiResponse => res.json(apiResponse.data))
+  .catch(err => console.log(err));
 });
 
     // https://www.mockaroo.com/docs
--- front-end/src/views/Portfolio/Portfolio.js
+++ front-end/src/views/Portfolio/Portfolio.js
@@ -1,6 +1,5 @@
 import './Portfolio.css';
 import React, { useState, useEffect, useMemo } from "react";
-import queryString from "query-string";
 import {
   LineChart,
   CartesianGrid,
@@ -13,109 +12,80 @@ import {
  
 } from "recharts";
 
-const API_TOKEN = "c8qd4eiad3ienapjjc9g";
-const CRYPTO_SYMBOLS = ["BINANCE:BTCUSDT", "BINANCE:ETHUSDT", "BINANCE:DOGEUSDT", "SOL", "BINANCE:SHIBUSDT"]
-const INTERVAL_OPTIONS = [30, 60, 90, 120]
-const API_URL = "https://finnhub.io/api/v1/crypto/candle"
-const RESOLUTION = "D"
+function Portfolio() {
+  const CRYPTO_SYMBOLS = ["BTC", "ETH", "USDT", "DOGE" ,"SHIB", "SOL"]
+  const INTERVAL_OPTIONS = [30, 60, 90, 120]
 
-
-function getUnixTime(date) {
-    return date.getTime() / 1000 | 0;
-  }
-  
   function transformData(data) {
     return data.c.map((item, index) => ({
-      close: Number(item).toFixed(2),
-      open: Number(data.o[index]).toFixed(2),
+      close: Number(item).toFixed(5),
+      open: Number(data.o[index]).toFixed(5),
       timestamp: new Date(data.t[index] * 1000).toLocaleDateString()
     }))
   }
+  const [symbol, setSymbol] = useState(CRYPTO_SYMBOLS[0])
+  const [data, setData] = useState(null);
+  const [interval, setInterval] = useState(INTERVAL_OPTIONS[0]);
 
+  useEffect(() => {
+    if (!symbol | !interval) {
+      return;
+    }
 
-  function Portfolio() {
-    const [symbol, setSymbol] = useState(CRYPTO_SYMBOLS[0])
-    const [data, setData] = useState(null);
-    const [interval, setInterval] = useState(INTERVAL_OPTIONS[0]);
+    fetch(`http://localhost:4000/graph/${symbol}/${interval}`)
+      .then(async data => setData(transformData(await data.json())))
+      .catch(error => console.error(error))
+
+  }, [symbol, interval]);
   
-    const to = useMemo(() => {
-      return getUnixTime(new Date());
-    }, []);
-    const from = useMemo(() => {
-      let d = new Date();
-      d.setDate(d.getDate() - interval);
+  const handleChangeCrypto = event =>
+        setSymbol(event.target.value);
+    
+  const handleChangeInterval = event =>
+        setInterval(event.target.value);
   
-      return getUnixTime(d);
-    }, [interval])
-  
-    useEffect(() => {
-      if (!from || !to || !symbol) {
-        return;
-      }
-  
-      const query = {
-        token: API_TOKEN,
-        resolution: RESOLUTION,
-        from,
-        to,
-        symbol
-      };
-  
-      fetch(`${API_URL}?${queryString.stringify(query)}`)
-        .then(async data => setData(transformData(await data.json())))
-        .catch(error => console.error(error))
-  
-    }, [from, to, symbol]);
-  
-const handleChangeCrypto = event =>
-      setSymbol(event.target.value);
-  
-const handleChangeInterval = event =>
-      setInterval(event.target.value);
-  
-return (
-    <>
-      <div id="page-title">
-        <h1>My Portfolio</h1>
-      </div>
-      <div id="page-content">
-        <div className="container">
-            <div className="selector">
-            <label for="stock_select" className="label">
-                <strong>Stock Symbol: </strong>
-            </label>
-            <select id="stock_select" onChange={handleChangeCrypto}>
-                {CRYPTO_SYMBOLS.map(s => <option key={s} value={s}>{s}</option>)}
-            </select>
-            </div>
-            <div className="selector">
-            <label for="interval_select" className="label">
-                <strong>Interval: </strong>
-            </label>
-            <select onChange={handleChangeInterval}>
-                {INTERVAL_OPTIONS.map(s => <option key={s} value={s}>Past {s} days</option>)}
-            </select>
-            </div>
+  return (
+      <>
+        <div id="page-title">
+          <h1>My Portfolio</h1>
         </div>
-        <div className="container">
-        <ResponsiveContainer width="100%" height={300}> 
-            <LineChart width={450} height={300} data={data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="timestamp" />
-            <YAxis type="number" allowDecimals={true}
-                allowDataOverflow={true} />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="open" stroke="blue" dot={false} />
-            <Line type="monotone" dataKey="close" stroke="gray" dot={false} />
-            </LineChart>
-        </ResponsiveContainer>
-        </div>
-      </div>   
-    </>
-);
-
-            }
+        <div id="page-content">
+          <div className="container dropdownContainer">
+              <div className="selector">
+              <label for="stock_select" className="label">
+                  <strong>Stock Symbol: </strong>
+              </label>
+              <select id="stock_select" onChange={handleChangeCrypto}>
+                  {CRYPTO_SYMBOLS.map(s => <option key={s} value={s}>{s}</option>)}
+              </select>
+              </div>
+              <div className="selector">
+              <label for="interval_select" className="label">
+                  <strong>Interval: </strong>
+              </label>
+              <select onChange={handleChangeInterval}>
+                  {INTERVAL_OPTIONS.map(s => <option key={s} value={s}>Past {s} days</option>)}
+              </select>
+              </div>
+          </div>
+          <div className="container">
+          <ResponsiveContainer width="100%" height={300}> 
+              <LineChart width={450} height={300} data={data}
+              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="timestamp" />
+              <YAxis type="number" allowDecimals={true}
+                  allowDataOverflow={true} />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="open" stroke="blue" dot={false} />
+              <Line type="monotone" dataKey="close" stroke="gray" dot={false} />
+              </LineChart>
+          </ResponsiveContainer>
+          </div>
+        </div>   
+      </>
+  );
+}
 
 export default Portfolio;
