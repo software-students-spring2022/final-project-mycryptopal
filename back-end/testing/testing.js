b5ccc77f21369e4d85e7f0d3d3e829eac7fb8c3a
Blake Rainey <brrainey13@gmail.com>
2022-04-05 15:13:54 -0400

    added more boilerplate code

--- back-end/testing/testing.js
+++ back-end/testing/testing.js
@@ -1,5 +1,3 @@
-import "./app.js"
-
 
 //boilerplate mocha code from their documentation/class
 // use node.js's built-in assert assertion library
@@ -16,5 +14,19 @@ describe("Array", function () {
   })
 })
 
+//boilerplate code of testing routes
 
+describe("GET request to /foo route", () => {
+    it("it should respond with an HTTP 200 status code and an object in the response body", done => {
+      chai
+        .request(server)
+        .get("/foo")
+        .end((err, res) => {
+          res.should.have.status(200) // use should to make BDD-style assertions
+          res.body.should.be.a("object") // our route sends back an object
+          res.body.should.have.property("success", true) // a way to check the exact value of a property of the response object
+          done() // resolve the Promise that these tests create so mocha can move on
+        })
+    })
+  })
 
