5f5730ad668f2fce0d1ad5b72cae2a9407d665e1
Hannah Harvard <harvard@nyu.edu>
2022-04-04 20:14:39 -0400

    added count /check num question feature to lesson template and created placeholder json format file for all question file and content

---
+++ back-end/Lessons/Lessons.js
@@ -0,0 +1,70 @@
+const lessons = [
+    {title:
+        [
+            {titleNum: 1, titleText: "Lesson 1"},
+            {titleNum: 2, titleText: "Lesson 2"},
+            {titleNum: 3, titleText: "Lesson 3"},
+            {titleNum: 4, titleText: "Lesson 4"},
+            {titleNum: 5, titleText: "Lesson 5"},
+            {titleNum: 6, titleText: "Lesson 6"},
+            {titleNum: 7, titleText: "Lesson 7"},
+            {titleNum: 8, titleText: "Lesson 8"},
+            {titleNum: 9, titleText: "Lesson 9"},
+            {titleNum: 10, titleText: "Lesson 10"}
+        ]
+    }           
+    {content: 
+        [
+            {content: 1, contentText: "Content Text"},
+            {content: 2, contentText: "Content Text"},
+            {content: 3, contentText: "Content Text"},
+            {content: 4, contentText: "Content Text"},
+            {content: 5, contentText: "Content Text"},
+            {content: 6, contentText: "Content Text"},
+            {content: 7, contentText: "Content Text"},
+            {content: 8, contentText: "Content Text"},
+            {content: 9, contentText: "Content Text"},
+            {content: 10, contentText: "Content Text"}
+
+        ]
+    }
+    {questions: 
+        [   
+            {questionNumber: 1, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 2, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 3, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 4, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 5, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 6, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 7, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 8, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 9, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 10, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 11, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 12, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 13, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 14, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 15, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 16, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 17, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 18, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 19, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 20, questionText: "Question Text", answer: "Question Answer"},
+            {questionNumber: 21, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 22, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 23, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 24, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 25, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 26, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 27, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 28, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 29, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 30, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 31, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 32, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 33, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 34, questionText: "Question Text", answer: "Question Answer"}, 
+            {questionNumber: 35, questionText: "Question Text", answer: "Question Answer"}  
+        ]
+    }
+]
