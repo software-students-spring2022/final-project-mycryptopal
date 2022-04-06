80d958dc060fa4550a55d4928b3e4e6992e58440
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-04-05 20:28:57 -0400

    Info properly reflects crypto history

--- front-end/src/views/Crypto/Crypto.js
+++ front-end/src/views/Crypto/Crypto.js
@@ -104,7 +104,7 @@ function Crypto() {
                             Cryptocurrency Information and History
                         </div>
                         <div id="history-content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            {info}
                         </div>
                     </div>
                 </div>
