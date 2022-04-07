aeb5ef371cfd317cf536f9f51d41a6f950dedd81
Hannah Harvard <harvard@nyu.edu>
2022-04-06 23:22:59 -0400

    added four lessons of content

--- back-end/Lessons/Lessons.js
+++ back-end/Lessons/Lessons.js
@@ -15,16 +15,36 @@ const lessons = [
     }           
     {content: 
         [
-            {content: 1, contentText: "Content Text"},
-            {content: 2, contentText: "Content Text"},
-            {content: 3, contentText: "Content Text"},
-            {content: 4, contentText: "Content Text"},
-            {content: 5, contentText: "Content Text"},
-            {content: 6, contentText: "Content Text"},
-            {content: 7, contentText: "Content Text"},
-            {content: 8, contentText: "Content Text"},
+            {content: 1, contentText: "A basic concept to note about the difference between the crypto market and stock market is that owning stock means ownership of a piece of a company. In a public offering for example, selling shares allows a company to raise more money. In some cases, even after a company is publicly traded, it may issue more stocks to raise money, but this dilutes the value of current shares."},
+            {content: 2, contentText: "On the other hand, cryptocurrencies have no intrinsic value as they are not backed by underlying assets or earnings. There are thousands of cryptocurrencies to choose from, and each of them are decentralized. In some cases, this creates volatility as they are not regulated. In some cases, countries such as China have banned crypto in hopes of keeping a regulated finance system. "},
+            {content: 3, contentText: "A blockchain is a distributed database that is shared among a computer network. This decentralized database is known as Distributed Ledger Technology (DLT). Essentially, a blockchain records information that makes it very hard for someone to change, hack, or steal."},
+            {content: 4, contentText: "A blockchain collects information in groups (called blocks), where each block has a certain  storage capacity. Different types of information can be stored on a blockchain, but the most common use is a ledger for transactions. Each transaction is recorded with a cryptographic signature called a hash. As such, if one block is changed (a transaction is made), it would be clear to see from the block history."},
+            {content: 5, contentText: "A cryptocurrency is a digital or virtual currency that is not backed by institutions and secured by cryptography. It is a form of digital asset based on a network that is distributed across a large number of computers. Specifically, cryptocurrencies run on a distributed public ledger (blockchain) that records all transactions for participants of a network to view. This means it is extremely hard to counterfeit or double-spend. Some benefits of a system like this are cheaper and faster money transfers and decentralization. "},
+            {content: 6, contentText: "To note: Owning a cryptocurrency does not mean owning the coin. As an owner of cryptocurrency, you do not own anything tangible. Instead, you own a key that allows you to move a record from one person to another without having to go through an institution such as a bank. "},
+            {content: 7, contentText: "A cryptocurrency exchange is a digital marketplace where you can buy and trade crypto. You cannot buy crypto from a bank or investing firm. Thus, once you decide on a cryptocurrency you would like to buy, you must create an account on a cryptocurrency exchange where you will exchange U.S. dollars for digital assets. On crypto exchanges, in addition to buying and selling, you have the option to convert one cryptocurrency for another. For example, if you have bitcoin and would like to convert a portion of your bitcoin to ethereum, this can be done on the exchange."},
+            {content: 8, contentText: "Some popular exchanges are Coinbase, Robinhood, and Binance. In order to choose the best crypto exchange, here are some things to keep note of: fees and minimum. "},
             {content: 9, contentText: "Content Text"},
             {content: 10, contentText: "Content Text"}
+            {content: 11, contentText: "Content Text"},
+            {content: 12, contentText: "Content Text"},
+            {content: 13, contentText: "Content Text"},
+            {content: 14, contentText: "Content Text"},
+            {content: 15, contentText: "Content Text"},
+            {content: 16, contentText: "Content Text"},
+            {content: 17, contentText: "Content Text"},
+            {content: 18, contentText: "Content Text"},
+            {content: 19, contentText: "Content Text"},
+            {content: 20, contentText: "Content Text"}
+            {content: 21, contentText: "Content Text"},
+            {content: 22, contentText: "Content Text"},
+            {content: 23, contentText: "Content Text"},
+            {content: 24, contentText: "Content Text"},
+            {content: 25, contentText: "Content Text"},
+            {content: 26, contentText: "Content Text"},
+            {content: 27, contentText: "Content Text"},
+            {content: 28, contentText: "Content Text"},
+            {content: 29, contentText: "Content Text"},
+            {content: 30, contentText: "Content Text"}
 
         ]
     }
