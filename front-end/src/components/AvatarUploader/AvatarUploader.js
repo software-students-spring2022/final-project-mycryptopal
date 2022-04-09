c41a73c68a4f3f932cc18da734796a0bec79e203
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-04-09 17:29:46 -0400

    Rewrote Contact Us page with Material UI components. Moved FAQ data to the back-end. Set up placeholder POST request in back-end for contact messages.

--- front-end/src/components/AvatarUploader/AvatarUploader.js
+++ front-end/src/components/AvatarUploader/AvatarUploader.js
@@ -1,51 +1,53 @@
 import './AvatarUploader.css';
 import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 function AvatarUploader(props) {
-    const fileDialog = useRef(null);
-    const [avatarURL, setAvatarURL] = useState('');
+  const fileDialog = useRef(null);
+  const [avatarURL, setAvatarURL] = useState('');
 
-    useEffect(() => {
-        if(props.userId){
-            async function getAvatarURL() {
-                const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/avatar/${props.userId}`);
-                const data = await res.json();
-                console.log(data);
-                setAvatarURL(`${process.env.REACT_APP_BACKEND_URL}/static/${data.url}`);
-              }
-            getAvatarURL();
-        }
-    }, [props.userId]);
-
-    function onOverlayClick() {
-       fileDialog.current.click();
-    };
-
-    function onFileChange(evt) {
-        const uploadFile = evt.target.files[0];
-        if(uploadFile.type.startsWith('image/')){
-            const uploadForm = new FormData();
-            uploadForm.append('userId', props.userId);
-            uploadForm.append('avatar', uploadFile);
-            axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/avatar`, uploadForm, {})
-            .then(res => {
-                window.location.href = '/settings';
-            })
-            .catch(err => console.log(err));    
-        };
+  useEffect(() => {
+    if (props.userId) {
+      async function getAvatarURL() {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/avatar/${props.userId}`);
+        const data = await res.json();
+        console.log(data);
+        setAvatarURL(`${process.env.REACT_APP_BACKEND_URL}/static/${data.url}`);
+      }
+      getAvatarURL();
     }
+  }, [props.userId]);
 
-    return (
-        <div id="avatar-uploader">
-            <input id="file-dialog" type='file' ref={fileDialog} accept="image/*" onChange={onFileChange}/>
-            <img id="current-pic" src={avatarURL} alt="avatar" onError={(evt) => {evt.target.src = 'https://picsum.photos/320'}} />
-            <div id="upload-overlay" onClick={onOverlayClick}>
+  function onOverlayClick() {
+    fileDialog.current.click();
+  };
+
+  function onFileChange(evt) {
+    const uploadFile = evt.target.files[0];
+    if (uploadFile.type.startsWith('image/')) {
+      const uploadForm = new FormData();
+      uploadForm.append('userId', props.userId);
+      uploadForm.append('avatar', uploadFile);
+      axios
+          .post(`${process.env.REACT_APP_BACKEND_URL}/avatar`, uploadForm, {})
+          .then((res) => {
+            window.location.href = '/settings';
+          })
+          .catch((err) => console.log(err));
+    };
+  }
+
+  return (
+    <div id="avatar-uploader">
+      <input id="file-dialog" type='file' ref={fileDialog} accept="image/*" onChange={onFileChange}/>
+      <img id="current-pic" src={avatarURL} alt="avatar" onError={(evt) => {
+        evt.target.src = 'https://picsum.photos/320';
+      }} />
+      <div id="upload-overlay" onClick={onOverlayClick}>
                 Change Profile Picture
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default AvatarUploader;
--- front-end/src/views/Settings/Settings.js
+++ front-end/src/views/Settings/Settings.js
@@ -1,7 +1,10 @@
 import {useEffect, useState} from 'react';
 import './Settings.css';
 import AvatarUploader from '../../components/AvatarUploader/AvatarUploader';
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 
 function Settings() {
   const [user, setUser] = useState({});
@@ -29,69 +32,69 @@ function Settings() {
             <AvatarUploader userId={user.id}/>
           </Grid>
 
-          <Grid item xs={12} md={3.25} id={"personalize-form"} className={"settingsForm"}>
+          <Grid item xs={12} md={3.25} id={'personalize-form'} className={'settingsForm'}>
             <form method='POST' action={`${process.env.REACT_APP_BACKEND_URL}/personalize`}>
-                <Grid container spacing={2}>
+              <Grid container spacing={2}>
 
-                  <Grid item xs={12}>
-                    <Typography variant='h5' marginBottom={'1vh'}>
+                <Grid item xs={12}>
+                  <Typography variant='h5' marginBottom={'1vh'}>
                       Personalize
-                    </Typography>
-                  </Grid>
-
-                  <Grid item xs={12} md={6}>
-                    <TextField name='firstName' id="fname-field" label="First Name" variant="outlined" placeholder={user.firstName} fullWidth InputLabelProps={{ shrink: true }}/>                     
-                  </Grid>
-
-                  <Grid item xs={12} md={6}>
-                    <TextField name='lastName' id="lname-field" label="Last Name" variant="outlined" placeholder={user.lastName} fullWidth InputLabelProps={{ shrink: true }}/>                               
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <TextField name='username' id="uname-field" label="Username" variant="outlined" placeholder={user.username} fullWidth InputLabelProps={{ shrink: true }}/>                               
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <TextField name='email' id="email-field" label="Email" variant="outlined" placeholder={user.email} fullWidth InputLabelProps={{ shrink: true }}/>                               
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Button variant="outlined" size='large' type='submit'>Submit</Button>
-                  </Grid>
+                  </Typography>
                 </Grid>
-              </form>
+
+                <Grid item xs={12} md={6}>
+                  <TextField name='firstName' id="fname-field" label="First Name" variant="outlined" placeholder={user.firstName} fullWidth InputLabelProps={{shrink: true}}/>
+                </Grid>
+
+                <Grid item xs={12} md={6}>
+                  <TextField name='lastName' id="lname-field" label="Last Name" variant="outlined" placeholder={user.lastName} fullWidth InputLabelProps={{shrink: true}}/>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField name='username' id="uname-field" label="Username" variant="outlined" placeholder={user.username} fullWidth InputLabelProps={{shrink: true}}/>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField name='email' id="email-field" label="Email" variant="outlined" placeholder={user.email} fullWidth InputLabelProps={{shrink: true}}/>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button variant="outlined" size='large' type='submit'>Submit</Button>
+                </Grid>
+              </Grid>
+            </form>
           </Grid>
 
           <Grid item xs={0} md={0.5}></Grid>
 
-          <Grid item xs={12} md={3.25} id={"security-form"} className={"settingsForm"}>
+          <Grid item xs={12} md={3.25} id={'security-form'} className={'settingsForm'}>
             <form method='POST' action={`${process.env.REACT_APP_BACKEND_URL}/security`}>
-                <Grid container spacing={2}>
+              <Grid container spacing={2}>
 
-                  <Grid item xs={12}>
-                    <Typography variant='h5' marginBottom={'1vh'}>
+                <Grid item xs={12}>
+                  <Typography variant='h5' marginBottom={'1vh'}>
                         Security
-                      </Typography>
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <TextField id="curr-pass-field" name="currentPassword" label="Current Password" variant="outlined" fullWidth type={'password'} InputLabelProps={{ shrink: true }} />                               
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <TextField id="new-pass-field" name="newPassword" label="New Password" variant="outlined" fullWidth type={'password'} InputLabelProps={{ shrink: true }} />                               
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <TextField id="re-pass-field" name="rePassword" label="Re-enter Password" variant="outlined" fullWidth type={'password'} InputLabelProps={{ shrink: true }} />                               
-                  </Grid>             
-
-                  <Grid item xs={12}>
-                    <Button variant="outlined" size='large' type='submit'>Submit</Button>
-                  </Grid>
-
+                  </Typography>
                 </Grid>
-              </form>
+
+                <Grid item xs={12}>
+                  <TextField id="curr-pass-field" name="currentPassword" label="Current Password" variant="outlined" fullWidth type={'password'} InputLabelProps={{shrink: true}} />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField id="new-pass-field" name="newPassword" label="New Password" variant="outlined" fullWidth type={'password'} InputLabelProps={{shrink: true}} />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField id="re-pass-field" name="rePassword" label="Re-enter Password" variant="outlined" fullWidth type={'password'} InputLabelProps={{shrink: true}} />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Button variant="outlined" size='large' type='submit'>Submit</Button>
+                </Grid>
+
+              </Grid>
+            </form>
           </Grid>
 
         </Grid>
