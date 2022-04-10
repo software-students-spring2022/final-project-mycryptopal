1467a8f9b9696dfd20de7d84b6eb9e452ff66479
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-04-09 22:41:24 -0400

    Added mongoTesting directory to test connections to server, created connection

---
+++ back-end/mongoTesting/schemas.js
@@ -0,0 +1,28 @@
+
+const { use } = require('chai');
+const mongoose = require('mongoose')
+
+const password = 'ivt4SmlFqu8avauz'
+
+const url = `mongodb+srv://Shanks480Agile:${password}@cluster0.wux8g.mongodb.net/myCryptoPal?retryWrites=true&w=majority
+`;
+
+mongoose.connect(url)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
