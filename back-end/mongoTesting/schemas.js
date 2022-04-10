7d4140668527e0364a361a515134a087c07e2277
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-04-09 23:25:55 -0400

    Added mock users to new users schema

--- back-end/mongoTesting/schemas.js
+++ back-end/mongoTesting/schemas.js
@@ -23,7 +23,51 @@ const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
     crypto: Array
+});
+
+const User = mongoose.model('User', UserSchema);
+
+const user1 = new User({
+    firstName: 'test',
+    lastName: 'user',
+    email: 'testUser123@gmail.com',
+    username: 'testUser123123',
+    password: 'testPassword12',
+    crypto: null
 })
+
+const user2 = new User({
+    firstName: 'test2',
+    lastName: 'user2',
+    email: 'testUser1233@gmail.com',
+    username: 'testUser1231123',
+    password: 'testPassword1132',
+    crypto: null
+})
+
+const user3 = new User({
+    firstName: 'test21212',
+    lastName: 'user2134134',
+    email: 'testUser1231343143@gmail.com',
+    username: 'testUser1781123',
+    password: 'testPassword156762',
+    crypto: null
+})
+
+user1.save(function(err, user) {
+    if (err) return console.error(err);
+    console.log(user1.username + " saved to atlas")
+});
+user2.save(function(err, user) {
+    if (err) return console.error(err);
+    console.log(user2.username + " saved to atlas")
+});
+user3.save(function(err, user) {
+    if (err) return console.error(err);
+    console.log(user3.username + " saved to atlas")
+});
+
+
 
 
 
