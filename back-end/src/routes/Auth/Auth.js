e0c6388eb478b5e8f41ae56ef0f120110ec58aa4
Blake Rainey <brrainey13@gmail.com>
2022-04-12 17:48:08 -0400

    added logout capability

--- back-end/src/routes/Auth/Auth.js
+++ back-end/src/routes/Auth/Auth.js
@@ -60,4 +60,9 @@ router.post('/login/password', passport.authenticate('local', {
   failureRedirect: '/login',
 }));
 
+router.post('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
+
 module.exports = router;
