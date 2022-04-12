578bc645ad6913447b0c6e51d6a6398865172081
Blake Rainey <brrainey13@gmail.com>
2022-04-12 09:44:24 -0400

    pushed some linting code changes

--- back-end/src/tests/crypto.test.js
+++ back-end/src/tests/crypto.test.js
@@ -1,7 +1,9 @@
+/* eslint no-unused-vars: "off", no-undef: "off", max-len: "off" */
+
 const app = require('../app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { expect } = require('chai');
+const {expect} = require('chai');
 const should = chai.should();
 chai.use(chaiHttp);
 
