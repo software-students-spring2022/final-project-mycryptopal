4eca924194142767a9c1f922b8b5320c1a8ff82f
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-04-17 19:40:30 -0400

    Configured news route to get articles from MongoDB instead of directly fetching from News API

--- back-end/src/routes/news/news.js
+++ back-end/src/routes/news/news.js
@@ -1,48 +1,20 @@
 const {Router} = require('express');
 const router = new Router({mergeParams: true});
+const Article = require('../../models/Article');
 const axios = require('axios');
 const path = require('path');
 require('dotenv').config({
   silent: true, path: path.join(__dirname, '../..', '.env'),
 }); // Stores custom environmental variables
 
-router.get('/news/feed', (req, res) => {
-  const SEARCH_TERM = 'cryptocurrency';
-  const SORT_BY = 'publishedAt';
-  const LANGUAGE = 'en';
-  const LIMIT = parseInt(req.query.limit);
-  const PAGE_SIZE = LIMIT ? LIMIT : 20;
-  const DOMAINS = [
-    'coindesk.com',
-    'cointelegraph.com',
-    'forbes.com',
-    'newsbtc.com',
-    'bitcoinmagazine.com',
-    'cryptoslate.com',
-    'bitcoinist.com',
-    'apnews.com',
-    'bbc.co.uk',
-    'economist.com',
-    'npr.org',
-    'u.today',
-    'cryptocynews.com',
-    'dailyhodl.com',
-    'cryptopolitan.com',
-  ];
-  axios
-      .get(
-          `${process.env.NEWS_API_URL}?apiKey=${process.env.NEWS_API_KEY}` +
-            `&q=${SEARCH_TERM}` +
-            `&language=${LANGUAGE}` +
-            `&sortBy=${SORT_BY}` +
-            `&pageSize=${PAGE_SIZE}` +
-            `&domains=${DOMAINS.join(',')}`,
-      )
-      .then((apiResponse) => res.json(apiResponse.data))
-      .catch((err) => {
-        res.status(400);
-        res.json({articles: []});
-      });
+router.get('/news/feed/', async (req, res) => {
+  try{
+    const articles = await Article.find({}, {_id: 0, __v: 0}, {sort: {publishDate: -1}});
+    res.json({success: true, articles: articles});
+  }
+  catch(err) {
+    res.status(500).json({success: false, error: 'Server error'});
+  }
 });
 
 module.exports = router;
