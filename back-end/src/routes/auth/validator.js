9994fb32b012f3e8eab653946043f43cbead572e
Blake Rainey <brrainey13@gmail.com>
2022-04-18 12:40:39 -0400

    added password rules

--- back-end/src/routes/auth/validator.js
+++ back-end/src/routes/auth/validator.js
@@ -4,7 +4,17 @@ const userValidationRules = () => {
     // username must be an email
     body('username').isEmail(),
     // password must be at least 5 chars long
-    body('password').isLength({min: 5}),
+    body('password')
+        .isString()
+        .isLength({min: 8})
+        .not()
+        .isLowercase()
+        .not()
+        .isUppercase()
+        .not()
+        .isNumeric()
+        .not()
+        .isAlpha(),
   ];
 };
 
