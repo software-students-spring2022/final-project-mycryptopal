d1bb58593bdcad3811308edd8e2495ada6a378b7
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-04-17 20:09:00 -0400

    Renamed schema for frequently asked questions

--- back-end/src/models/FAQ.js
+++ back-end/src/models/FAQ.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose');
 const faqSchema = new mongoose.Schema({
   question: String,
   answer: String,
 });
 
-const FAQ = mongoose.model('FAQ', schema);
+const FAQ = mongoose.model('FAQ', faqSchema);
 
 module.exports = FAQ;
