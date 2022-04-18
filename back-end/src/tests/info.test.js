8ee78cd03649a6e42dcccaad06e645200e0232ae
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-04-18 00:35:17 -0400

    Created test file for /info routes

---
+++ back-end/src/tests/info.test.js
@@ -0,0 +1,15 @@
+/* eslint no-unused-vars: "off", no-undef: "off", max-len: "off" */
+const app = require('../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Testing /info routes', () => {
+  describe('GET /info/faq', async () => {
+
+  });
+  describe('POST /info/contact', async () => {
+
+  });
+});
