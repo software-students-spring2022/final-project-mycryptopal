296a2a5f0ab8b0b99304518cc768a95fb3f4646c
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-04-18 00:20:23 -0400

    Wrote tests for news route

--- back-end/src/routes/news/news.js
+++ back-end/src/routes/news/news.js
@@ -6,12 +6,18 @@ require('dotenv').config({
   silent: true, path: path.join(__dirname, '../..', '.env'),
 }); // Stores custom environmental variables
 
-router.get('/news/feed/', async (req, res) => {
-  try {
-    const articles = await Article.find({}, {_id: 0, __v: 0}, {sort: {publishDate: -1}});
-    res.json({success: true, articles: articles});
-  } catch (err) {
-    res.status(500).json({success: false, error: 'Server error'});
+router.get('/news/feed', async (req, res) => {
+  const limit = parseInt(req.query.limit);
+  if (!limit || limit <= 0) {
+    res.status(400).json({success: false, error: 'Invalid limit'});
+  } else {
+    try {
+      const articles = await Article.find({}, {_id: 0, __v: 0}, {sort: {publishDate: -1}, limit: limit});
+      res.json({success: true, articles: articles});
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({success: false, error: 'Server error'});
+    }
   }
 });
 
