9f9e0be09fe7399f5a4c24a0b8b369ab6c42e925
Hannah Harvard <hh1927@nyu.edu>
2022-04-21 11:47:03 -0400

    opening working tests

--- back-end/src/tests/crypto.test.js
+++ back-end/src/tests/crypto.test.js
@@ -1,13 +1,13 @@
 /* eslint no-unused-vars: "off", no-undef: "off", max-len: "off" */
 
-// const app = require('../app');
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const {expect} = require('chai');
-// const should = chai.should();
-// chai.use(chaiHttp);
+ const app = require('../app');
+ const chai = require('chai');
+ const chaiHttp = require('chai-http');
+ const {expect} = require('chai');
+ const should = chai.should();
+ chai.use(chaiHttp);
 
-// // Tests for external API calls
+//  Tests for external API calls
 
 // describe('GET /api/crypto/explore', () => {
 //   it('should respond with an object containing a default number of cryptocurrencies along with their basic information', (done) => {
@@ -52,58 +52,58 @@
 //   });
 // });
 
-// describe('GET /api/crypto/data/:symbol', () => {
-//   it('should respond with an object containing a cryptocurrency\'s latest market quote given that the provided symbol is valid', (done) => {
-//     chai
-//         .request(app)
-//         .get('/api/crypto/data/btc')
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.be.a('object');
-//           res.body.should.include.all.keys('circulating_supply', 'quote');
-//           res.body['quote'].should.have.property('USD');
-//           res.body['quote']['USD'].should.include.all.keys('price', 'volume_24h', 'percent_change_24h', 'market_cap');
-//           done();
-//         });
-//   });
-//   it('should respond with a 404 error code and an empty response object if the provided symbol is invalid', (done) => {
-//     chai
-//         .request(app)
-//         .get('/api/crypto/data/thisCoinDoesNotExist')
-//         .end((err, res) => {
-//           res.should.have.status(404);
-//           res.body.should.be.a('object');
-//           res.body.should.be.empty;
-//           done();
-//         });
-//   });
-// });
+ describe('GET /api/crypto/data/:symbol', () => {
+   it('should respond with an object containing a cryptocurrency\'s latest market quote given that the provided symbol is valid', (done) => {
+     chai
+         .request(app)
+         .get('/api/crypto/data/btc')
+         .end((err, res) => {
+           res.should.have.status(200);
+           res.body.should.be.a('object');
+           res.body.should.include.all.keys('circulating_supply', 'quote');
+           res.body['quote'].should.have.property('USD');
+           res.body['quote']['USD'].should.include.all.keys('price', 'volume_24h', 'percent_change_24h', 'market_cap');
+           done();
+         });
+   });
+   it('should respond with a 404 error code and an empty response object if the provided symbol is invalid', (done) => {
+     chai
+         .request(app)
+         .get('/api/crypto/data/thisCoinDoesNotExist')
+         .end((err, res) => {
+           res.should.have.status(404);
+           res.body.should.be.a('object');
+           res.body.should.be.empty;
+           done();
+         });
+   });
+ });
 
-// describe('GET /api/crypto/info/:symbol', () => {
-//   it('should respond with an object containing a cryptocurrency\'s metadata given that the provided symbol is valid, in which the cryptocurrency\'s description can be found', (done) => {
-//     chai
-//         .request(app)
-//         .get('/api/crypto/info/btc')
-//         .end((err, res) => {
-//           res.should.have.status(200);
-//           res.body.should.be.a('object');
-//           res.body.should.have.property('description');
-//           res.body['description'].should.be.a('string');
-//           done();
-//         });
-//   });
-//   it('should respond with a 404 error code and an empty response object if the provided symbol is invalid', (done) => {
-//     chai
-//         .request(app)
-//         .get('/api/crypto/info/thisCoinDoesNotExist')
-//         .end((err, res) => {
-//           res.should.have.status(404);
-//           res.body.should.be.a('object');
-//           res.body.should.be.empty;
-//           done();
-//         });
-//   });
-// });
+ describe('GET /api/crypto/info/:symbol', () => {
+   it('should respond with an object containing a cryptocurrency\'s metadata given that the provided symbol is valid, in which the cryptocurrency\'s description can be found', (done) => {
+     chai
+         .request(app)
+         .get('/api/crypto/info/btc')
+         .end((err, res) => {
+           res.should.have.status(200);
+           res.body.should.be.a('object');
+           res.body.should.have.property('description');
+           res.body['description'].should.be.a('string');
+           done();
+         });
+   });
+   it('should respond with a 404 error code and an empty response object if the provided symbol is invalid', (done) => {
+     chai
+         .request(app)
+         .get('/api/crypto/info/thisCoinDoesNotExist')
+         .end((err, res) => {
+           res.should.have.status(404);
+           res.body.should.be.a('object');
+           res.body.should.be.empty;
+           done();
+         });
+   });
+ });
 
 // describe('GET /api/crypto/graph/:symbol', () => {
 //   it('should respond with an object containing a cryptocurrency\'s metadata given that the provided symbol is valid, in which the cryptocurrency\'s price data and graph can be found', (done) => {
