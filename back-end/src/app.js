377fe810c98db719165351299ffddedb3535fd10
Nguyen Dang <dangdinhnguyen2000@gmail.com>
2022-04-22 11:48:08 -0400

    Removed placeholder route in app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -47,17 +47,4 @@ app.use(morgan('dev')); // Sets logging mode
 app.use(cors()); // Enables CORS
 app.use('/', router);
 
-// Placeholder/not important yet
-app.get('/assets', (req, res) => {
-  const COINS = ['BTC', 'ETH', 'DOGE', 'SOL', 'XMR'];
-  const FRACTIONS = new Array(COINS.length).fill(0).map(() => {
-    return parseInt((Math.random() * 200)) + 20;
-  });
-  const ALLOCATIONS = COINS.reduce((current, element, index) => {
-    current[element] = FRACTIONS[index];
-    return current;
-  }, {});
-  res.json(ALLOCATIONS);
-});
-
 module.exports = app;
