3ca5844d188ff9d062375c6b006dfca072e33d16
Blake Rainey <brrainey13@gmail.com>
2022-05-01 20:47:40 -0400

    pushing some code lint changes

--- front-end/src/components/LessonQuiz/LessonQuiz.js
+++ front-end/src/components/LessonQuiz/LessonQuiz.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useState, useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 import './LessonQuiz.css';
--- front-end/src/views/Home/Home.js
+++ front-end/src/views/Home/Home.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useState, useEffect} from 'react';
 import './Home.css';
 import NewsFeed from '../../components/NewsFeed/NewsFeed';
