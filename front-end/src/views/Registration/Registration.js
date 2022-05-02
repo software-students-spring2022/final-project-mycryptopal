847d86ef7f65a7c0d83d3f7a809e2c588ff7b52a
Shashanka-Sharma <81830507+Shashanka-Sharma@users.noreply.github.com>
2022-05-02 01:09:10 -0400

    added alert library

--- front-end/src/views/Registration/Registration.js
+++ front-end/src/views/Registration/Registration.js
@@ -7,6 +7,7 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
 import axios from 'axios';
 
 function Registration() {
